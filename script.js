db9c2e1f4bd36a80ac58fee6bc899fe5f6392f97
romilpolii <romilpolii25@gmail.com>
2020-12-07 23:42:14 +0800

    selesai

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click',function(){
     nav.classList.toggle('slide');
 });
+
+const txtElement = ['Curriculum Vitae'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+	if(count == txtElement.length){
+		count = 0;
+	}
+
+	currentTxt = txtElement[count];
+
+	words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik1').textContent = words;
+    document.querySelector('.efek-ngetik2').textContent = words;
+
+	if(words.length == currentTxt.length){
+		count++;
+		txtIndex = 0;
+	}
+
+	setTimeout(ngetik, 300);
+
+})();
